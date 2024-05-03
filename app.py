0c1ee80eaa9838a975f85e28421fa1d2a34bb31b
kevinzhangTT <kevin9019888@gmail.com>
2024-05-03 01:02:05 -0600

    update main app to display spinning cube with texture (texture not working)

--- app.py
+++ app.py
@@ -2,17 +2,20 @@ import pygame
 from OpenGL.GL import *
 from OpenGL.GL.shaders import compileProgram, compileShader
 import numpy as np
+import pyrr
 import time
 import math
 import random
 import ctypes
+
+APP_SIZE = (1280, 720)
 
 class App:
 
     def __init__(self) -> None:
         # Initialize pygame
         pygame.init()
-        pygame.display.set_mode((1280, 720), pygame.OPENGL | pygame.DOUBLEBUF)
+        pygame.display.set_mode((APP_SIZE[0], APP_SIZE[1]), pygame.OPENGL | pygame.DOUBLEBUF)
         self.clock = pygame.time.Clock()
 
         # Initialize OpenGL
@@ -20,6 +23,7 @@ class App:
         
         # Enable and set up blending for transparency
         glEnable(GL_BLEND)
+        glEnable(GL_DEPTH_TEST)
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
         
         # Load and use shaders from files
@@ -29,10 +33,29 @@ class App:
         # Set texture unit 0 as active uniform sampler location for texture named 'imageTexture' in fragment shader
         glUniform1i(glGetUniformLocation(self.shader, "imageTexture"), 0)
         
-        self.triangle = Triangle()
+        # Define cube
+        self.cube = Cube(
+            position=[0, 0, -3],
+            eulers=[0, 0, 0]
+        )
         
-        # Load glitch texture
-        self.image_texture = Material("images/me.jpg")
+        self.cube_mesh = CubeMesh()
+        
+        # Load texture image
+        self.image_texture = Material("images/wood.jpeg")
+        
+        # Define a 4x4 projection transform with params
+        projection_transform = pyrr.matrix44.create_perspective_projection(
+            fovy=45, aspect=APP_SIZE[0]/APP_SIZE[1], near=0.1, far=10, dtype=np.float32
+        )
+        
+        #! add comment
+        glUniformMatrix4fv(
+            glGetUniformLocation(self.shader, "projection"), 1, GL_FALSE, projection_transform
+        )
+        
+        # Get location in shader where model matrix should go and stores it for efficiency
+        self.model_matrix_location = glGetUniformLocation(self.shader, "model")
         
         self.main_loop()
 
@@ -55,54 +78,128 @@ class App:
         running = True
 
         while running:
-            current_time = time.time() - start_time
+            # current_time = time.time() - start_time
             
             # Check events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                    
+            # Update cube
+            self.cube.eulers[2] += 1
+            if (self.cube.eulers[2] > 360):
+                self.cube.eulers[2] -= 360
 
             # Refresh screen
-            glClear(GL_COLOR_BUFFER_BIT)
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
             
-            # Use shader program and bind VAO for triangle
+            # Use shader program and bind VAO for cube
             glUseProgram(self.shader)
             self.image_texture.use()
-            glBindVertexArray(self.triangle.vao)
             
-            self.triangle.update_colour(current_time)
-            self.triangle.update_movement(current_time)
+            # Create identity, multiply transformations progressively
+            model_transform = pyrr.matrix44.create_identity(dtype=np.float32)
             
-            # Draw triangle using currently bound shader and VAO
-            glDrawArrays(GL_TRIANGLE_STRIP, 0, self.triangle.vertex_count)
+            # Rotate cube around its own axis
+            model_transform = pyrr.matrix44.multiply(
+                m1=model_transform,
+                m2=pyrr.matrix44.create_from_eulers(
+                    eulers=np.radians(self.cube.eulers),
+                    dtype=np.float32
+                )
+            )
+            
+            # Translate cube to its position
+            model_transform = pyrr.matrix44.multiply(
+                m1=model_transform,
+                m2=pyrr.matrix44.create_from_translation(
+                    vec=self.cube.position,
+                    dtype=np.float32
+                )
+            )
+            
+            glUniformMatrix4fv(self.model_matrix_location, 1, GL_FALSE, model_transform)
+            
+            glBindVertexArray(self.cube_mesh.vao)
+            
+            # Draw cube using currently bound shader and VAO
+            glDrawArrays(GL_TRIANGLES, 0, self.cube_mesh.vertex_count)
             
             pygame.display.flip()
 
             # Timing
-            self.clock.tick(360)
+            self.clock.tick(60)
 
         self.quit()
 
     def quit(self):
-        self.triangle.delete()
+        self.cube_mesh.delete()
         self.image_texture.delete()
         glDeleteProgram(self.shader)
         pygame.quit()
 
-class Triangle:
+class Cube:
     
-    def __init__(self) -> None:
-        self.vertex_count = 3
-        
-        # convert to type readable by graphics card
-        # x, y, z, r, g, b, s, t
+    def __init__(self, position, eulers) -> None:
+        self.position = np.array(position, dtype=np.float32)
+        self.eulers = np.array(eulers, dtype=np.float32)
+
+class CubeMesh:
+    
+    def __init__(self) -> None:# convert to type readable by graphics card
+        # x, y, z, s, t
         self.vertices = np.array([
-             0.0,   0.25, 0.0, 0.0, 0.0, 1.0, 0.5, 0.0,
-            -0.25, -0.25, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0,
-             0.25, -0.25, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0
-        ], dtype = np.float32)
+            -0.5, -0.5, -0.5, 0, 0,
+             0.5, -0.5, -0.5, 1, 0,
+             0.5,  0.5, -0.5, 1, 1,
+
+             0.5,  0.5, -0.5, 1, 1,
+            -0.5,  0.5, -0.5, 0, 1,
+            -0.5, -0.5, -0.5, 0, 0,
+
+            -0.5, -0.5,  0.5, 0, 0,
+             0.5, -0.5,  0.5, 1, 0,
+             0.5,  0.5,  0.5, 1, 1,
+
+             0.5,  0.5,  0.5, 1, 1,
+            -0.5,  0.5,  0.5, 0, 1,
+            -0.5, -0.5,  0.5, 0, 0,
+
+            -0.5,  0.5,  0.5, 1, 0,
+            -0.5,  0.5, -0.5, 1, 1,
+            -0.5, -0.5, -0.5, 0, 1,
+
+            -0.5, -0.5, -0.5, 0, 1,
+            -0.5, -0.5,  0.5, 0, 0,
+            -0.5,  0.5,  0.5, 1, 0,
+
+             0.5,  0.5,  0.5, 1, 0,
+             0.5,  0.5, -0.5, 1, 1,
+             0.5, -0.5, -0.5, 0, 1,
+
+             0.5, -0.5, -0.5, 0, 1,
+             0.5, -0.5,  0.5, 0, 0,
+             0.5,  0.5,  0.5, 1, 0,
+
+            -0.5, -0.5, -0.5, 0, 1,
+             0.5, -0.5, -0.5, 1, 1,
+             0.5, -0.5,  0.5, 1, 0,
+
+             0.5, -0.5,  0.5, 1, 0,
+            -0.5, -0.5,  0.5, 0, 0,
+            -0.5, -0.5, -0.5, 0, 1,
+
+            -0.5,  0.5, -0.5, 0, 1,
+             0.5,  0.5, -0.5, 1, 1,
+             0.5,  0.5,  0.5, 1, 0,
+
+             0.5,  0.5,  0.5, 1, 0,
+            -0.5,  0.5,  0.5, 0, 0,
+            -0.5,  0.5, -0.5, 0, 1,
+        ], dtype=np.float32)
         
-        self.vertex_stride = 8
+        self.vertex_stride = 5
+        self.vertex_count = len(self.vertices) // 5
         
         # create VAO and VBO and binds them
         self.vao = glGenVertexArrays(1)
@@ -115,56 +212,11 @@ class Triangle:
         
         # set up position attribute
         glEnableVertexAttribArray(0)
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(0))
-        
-        # set up colour attribute
-        glEnableVertexAttribArray(1)
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(12))
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 20, ctypes.c_void_p(0))
         
         # set up texture attribute
         glEnableVertexAttribArray(2)
-        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(24))
-        
-        self.movement_direction = 1
-        self.movement_angle = (random.randrange(0.0, 1.0), random.randrange(0.0, 1.0))
-        
-    def update_movement(self, time):
-        movement_speed = 5 * time
-        radius = 0.2
-        
-        # Update vertex positions to move in a circle
-        self.vertices[0 * self.vertex_stride + 0] = math.sin(movement_speed * time + 3.0) * radius + 0.00
-        self.vertices[0 * self.vertex_stride + 1] = math.cos(movement_speed * time + 3.0) * radius + 0.75
-        self.vertices[1 * self.vertex_stride + 0] = math.sin(movement_speed * time + 1.0) * radius - 0.75
-        self.vertices[1 * self.vertex_stride + 1] = math.cos(movement_speed * time + 1.0) * radius - 0.75
-        self.vertices[2 * self.vertex_stride + 0] = math.sin(movement_speed * time + 2.0) * radius + 0.75
-        self.vertices[2 * self.vertex_stride + 1] = math.cos(movement_speed * time + 2.0) * radius - 0.75
-        
-        
-    def update_colour(self, time):        
-        colour_speed = 4
-        brightness = 1
-        
-        # Update colors to cycle through RGB smoothly
-        self.vertices[0 * self.vertex_stride + 3 : 0 * self.vertex_stride + 6] = (
-            brightness * (0.5 * math.sin(colour_speed * time + 0 * math.pi / 3)+ 0.5),
-            brightness * (0.5 * math.sin(colour_speed * time + 2 * math.pi / 3)+ 0.5),
-            brightness * (0.5 * math.sin(colour_speed * time + 4 * math.pi / 3)+ 0.5)
-        )
-        self.vertices[1 * self.vertex_stride + 3 : 1 * self.vertex_stride + 6] = (
-            brightness * (0.5 * math.sin(colour_speed * time + 2 * math.pi / 3)+ 0.5),
-            brightness * (0.5 * math.sin(colour_speed * time + 4 * math.pi / 3)+ 0.5),
-            brightness * (0.5 * math.sin(colour_speed * time + 0 * math.pi / 3)+ 0.5)
-        )
-        self.vertices[2 * self.vertex_stride + 3 : 2 * self.vertex_stride + 6] = (
-            brightness * (0.5 * math.sin(colour_speed * time + 4 * math.pi / 3)+ 0.5),
-            brightness * (0.5 * math.sin(colour_speed * time + 0 * math.pi / 3)+ 0.5),
-            brightness * (0.5 * math.sin(colour_speed * time + 2 * math.pi / 3)+ 0.5)
-        )
-        
-        # Update the VBO with the new vertex data
-        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
-        glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_DYNAMIC_DRAW)
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 20, ctypes.c_void_p(12))
         
     def delete(self):
         # delete VAO and VBO
